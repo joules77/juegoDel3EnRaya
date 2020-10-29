f80082650805fac02670ab0918efaad61dc7377c
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-10-29 15:52:16 -0700

    inicializamos con un metodo con sus primeras variables

---
+++ src/juegoDel3EnRaya/juego3EnRaya.java
@@ -0,0 +1,18 @@
+package juegoDel3EnRaya;
+
+public class juego3EnRaya {
+
+	public static void main(String[] args) {
+		jugar();
+
+	}
+	/*
+	 * metodo donde comenzaremos a jugar
+	 * */
+	public static void jugar() {
+		//representaciones de los jugadores y del simbolo vacio
+		char J1 = 'X';
+		char J2 = 'O'; 
+	}
+
+}
